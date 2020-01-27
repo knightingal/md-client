3e9f6a7b15146babe31d419c830dc60ca88717eb
Knightingal <yujianming1985@hotmail.com>
2020-01-27 14:47:14 +0800

    fix sectionContent.tsx indent to 2

--- src/pages/flow1000/sectionContent.tsx
+++ src/pages/flow1000/sectionContent.tsx
@@ -6,99 +6,100 @@ import ImgComponent  from '../../components/ImgComponent';
 import {Flow1000ModelState} from '../../models/flow1000';
 import { connect } from 'dva';
 class SectionDetail {
-    dirName:string;
-    picPage:string;
-    pics:Array<ImgDetail>;
+  dirName:string;
+  picPage:string;
+  pics:Array<ImgDetail>;
 
-    constructor() {
-        this.dirName = "";
-        this.picPage = "";
-        this.pics = [];
-    }
+  constructor() {
+    this.dirName = "";
+    this.picPage = "";
+    this.pics = [];
+  }
 }
 
 class ImgDetail implements HeightType{
-    name:string;
-    width:number;
-    height:number;
+  name:string;
+  width:number;
+  height:number;
 
-    constructor(name:string, width:number, height:number) {
-        this.name = name;
-        this.width = width;
-        this.height = height;
-    }
+  constructor(name:string, width:number, height:number) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+  }
 }
 
 class Content extends React.Component<{index:number, password:string, height: number}, {sectionDetail:SectionDetail, scrollTop:number}>  {
-    constructor(props:{index: number, password:string, height: number}) {
-        super(props);
-        this.state = {sectionDetail: new SectionDetail(), scrollTop:0};
+  constructor(props:{index: number, password:string, height: number}) {
+    super(props);
+    this.state = {sectionDetail: new SectionDetail(), scrollTop:0};
+  }
+  
+  fecthSectionList(index: number) {
+    if (index <= 0) {
+      return;
     }
-    
-    fecthSectionList(index: number) {
-        if (index <= 0) {
-            return;
-        }
-        fetch(`/local1000/picDetailAjax?id=${index}`)
-        .then((resp: Response) => {
-            return resp.json();
-        })
-        .then((json: any) => {
-            const sectionDetail:SectionDetail = json;
-            this.setState({
-                sectionDetail:sectionDetail,
-                scrollTop:0
-            });
-        });
+    fetch(`/local1000/picDetailAjax?id=${index}`)
+    .then((resp: Response) => {
+      return resp.json();
+    })
+    .then((json: any) => {
+      const sectionDetail:SectionDetail = json;
+      this.setState({
+        sectionDetail:sectionDetail,
+        scrollTop:0
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.fecthSectionList(this.props.index);        
+  }
+
+
+  componentDidUpdate(prevProps: {index:number}) {
+    if (this.props.index !== prevProps.index) {
+      this.fecthSectionList(this.props.index);        
     }
+  }
 
-    componentDidMount() {
-        this.fecthSectionList(this.props.index);        
-    }
-
-
-    componentDidUpdate(prevProps: {index:number}) {
-        if (this.props.index !== prevProps.index) {
-            this.fecthSectionList(this.props.index);        
-        }
-
-    }
-
-    render() {
-        return <LazyLoader height={this.props.height - 64} dataList={this.state.sectionDetail.pics} parentComp={this} scrollTop={this.state.scrollTop}/>
-    }
+  render() {
+    return <LazyLoader height={this.props.height - 64} dataList={this.state.sectionDetail.pics} parentComp={this} scrollTop={this.state.scrollTop}/>
+  }
 };
 
 export default connect(({flow1000}:{flow1000: Flow1000ModelState}) => {
-    const props = {
-        height: flow1000.height,
-        index: flow1000.sectionIndex,
-        password: ""
-    }
-    return props;
+  const props = {
+    height: flow1000.height,
+    index: flow1000.sectionIndex,
+    password: ""
+  }
+  return props;
 })(Content);
 
 class ImgComponentItem extends React.Component<{mount: boolean, item: ImgDetail, parentComp:Content}> {
-    constructor(props:{item: ImgDetail, parentComp:Content, mount: boolean}) {
-        super(props);
-    }
+  constructor(props:{item: ImgDetail, parentComp:Content, mount: boolean}) {
+    super(props);
+  }
 
-    render() {
-        return <ImgComponent 
-            width={this.props.item.width} 
-            height={this.props.item.height} 
-            src={`/static/encrypted/${this.props.parentComp.state.sectionDetail.dirName}/${this.props.item.name}.bin`} 
-            password="yjmK14040842$000" 
-        /> 
-    }
+  render() {
+    return (
+      <ImgComponent 
+        width={this.props.item.width} 
+        height={this.props.item.height} 
+        src={`/static/encrypted/${this.props.parentComp.state.sectionDetail.dirName}/${this.props.item.name}.bin`} 
+        password="yjmK14040842$000" 
+      /> 
+    );
+  }
 }
 const LazyLoader: 
-    React.ComponentClass<
-        LazyProps<
-            ImgDetail, 
-            {index:number, password:string}, 
-            {}, 
-            Content
-        >
-    > 
+  React.ComponentClass<
+    LazyProps<
+      ImgDetail, 
+      {index:number, password:string}, 
+      {}, 
+      Content
+    >
+  > 
 = lazyLoader(ImgComponentItem, "Content", 2)
