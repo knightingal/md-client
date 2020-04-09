339308c29e4e454793f6a4dd0d2a541bdb7150b5
Knightingal <yujianming1985@hotmail.com>
2020-04-09 11:29:55 +0800

    functional ImgComponent

--- src/components/ImgComponent.tsx
+++ src/components/ImgComponent.tsx
@@ -1,59 +1,46 @@
 import * as React from 'react';
-import {decryptArray} from '../lib/decryptoArray';
+import { decryptArray } from '../lib/decryptoArray';
 import { connect } from 'dva';
 import { Flow1000ModelState } from '@/models/flow1000';
+import { useState, useEffect } from 'react';
+
+interface ImgComponentProp {
+  src: string;
+  height: number;
+  width: number;
+  password: string;
+}
+
+const ImgComponent = (props: ImgComponentProp) => {
+  const [url, setUrl] = useState<string | null>(null);
+  const fetchImgByUrl = (url: string) => {
+    fetch(url)
+      .then(response => {
+        return response.arrayBuffer();
+      })
+      .then(arrayBuffer => {
+        const decrypted = decryptArray(arrayBuffer, props.password);
+        const objectURL = URL.createObjectURL(new Blob([decrypted]));
+        setUrl(objectURL);
+      });
+  };
+  useEffect(() => {
+    if (props.src != null) {
+      fetchImgByUrl(props.src);
+      setUrl(null);
+    }
+  }, [props.src]);
+
+  return (
+    <img
+      src={url == null ? '' : url}
+      style={{ display: 'block', margin: 'auto' }}
+      height={`${props.height}px`}
+      width={`${props.width}px`}
+    />
+  );
+};
 
 export default connect(({ flow1000 }: { flow1000: Flow1000ModelState }) => {
-  return { password: flow1000.pwd, };
-}) (
-class ImgComponent extends React.Component<{src: string, height: number, width: number, password: string}, {url: string|null}> {
-    constructor(props:{src: string, height: number, width: number, password: string}) {
-        super(props);
-        this.state = {
-            url:null
-        }
-    }
-
-    fetchImgByUrl(url: string) {
-        fetch(url).then(response => {
-            return response.arrayBuffer();
-        }).then(arrayBuffer => {
-            const decrypted = decryptArray(arrayBuffer, this.props.password);
-            const objectURL = URL.createObjectURL(new Blob([decrypted]));
-            this.setState({
-                url: objectURL,
-            });
-        });
-    }
-
-    componentDidMount() {
-        if (this.props.src != null) {
-            this.fetchImgByUrl(this.props.src);
-            this.setState({
-                url: null
-            });
-        }
-
-    }
-
-    componentDidUpdate(prevProps: {src: string}) {
-        if (this.props.src !== prevProps.src) {
-            if(this.props.src != null) {
-                this.fetchImgByUrl(this.props.src);
-                this.setState({
-                    url: null
-                });
-            }
-        }
-    }
-
-    render() {
-        return <img 
-            src={this.state.url==null?"":this.state.url} 
-            style={{display:"block", margin:'auto' }}
-            height={`${this.props.height}px`} 
-            width={`${this.props.width}px`}
-        />
-    }
-}
-)
+  return { password: flow1000.pwd };
+})(ImgComponent);
