d3a0fef3a0edacf48fc00740406d415c828efc01
御铭 <yujianming1985@126.com>
2023-01-26 00:02:48 +0800

    add search imput

--- src/layouts/flow1000.tsx
+++ src/layouts/flow1000.tsx
@@ -10,6 +10,9 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { Outlet } from "react-router-dom";
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 
 interface Flow1000Props {
     height:number;
@@ -52,11 +55,19 @@ export default connect(
                     <Typography 
                         variant="h6" 
                         component="div" 
-                        sx={{flexGlow:1}}
+                        sx={{flexGrow:1}}
                     >
                         Welcome to user Flow1000
                     </Typography>
-                    <Button color="inherit">About</Button>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
                 </Toolbar>
             </AppBar>
         </Box>
@@ -67,4 +78,46 @@ export default connect(
     </div>
     );
 });
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
