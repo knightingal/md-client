12e2c7bc61abbbbcea73fbc024bb84402da04aa7
御铭 <yujianming1985@126.com>
2023-02-19 10:27:33 +0800

    format code

--- src/components/ImgComponent.tsx
+++ src/components/ImgComponent.tsx
@@ -1,55 +1,55 @@
 import * as React from 'react';
-import {decryptArray} from '../lib/decryptoArray';
+import { decryptArray } from '../lib/decryptoArray';
 
 
-export default class ImgComponent extends React.Component<{src: string, height: number, width: number, password: string}, {url: string|null}> {
-    constructor(props:{src: string, height: number, width: number, password: string}) {
-        super(props);
-        this.state = {
-            url:null
-        }
+export default class ImgComponent extends React.Component<{ src: string, height: number, width: number, password: string }, { url: string | null }> {
+  constructor(props: { src: string, height: number, width: number, password: string }) {
+    super(props);
+    this.state = {
+      url: null
+    }
+  }
+
+  fetchImgByUrl(url: string) {
+    fetch(url).then(response => {
+      return response.arrayBuffer();
+    }).then(arrayBuffer => {
+      const decrypted = decryptArray(arrayBuffer, this.props.password);
+      // const decrypted = arrayBuffer;
+      const objectURL = URL.createObjectURL(new Blob([decrypted]));
+      this.setState({
+        url: objectURL,
+      });
+    });
+  }
+
+  componentDidMount() {
+    if (this.props.src != null) {
+      this.fetchImgByUrl(this.props.src);
+      this.setState({
+        url: null
+      });
     }
 
-    fetchImgByUrl(url: string) {
-        fetch(url).then(response => {
-            return response.arrayBuffer();
-        }).then(arrayBuffer => {
-            const decrypted = decryptArray(arrayBuffer, this.props.password);
-            // const decrypted = arrayBuffer;
-            const objectURL = URL.createObjectURL(new Blob([decrypted]));
-            this.setState({
-                url: objectURL,
-            });
+  }
+
+  componentDidUpdate(prevProps: { src: string }) {
+    if (this.props.src !== prevProps.src) {
+      if (this.props.src != null) {
+        this.fetchImgByUrl(this.props.src);
+        this.setState({
+          url: null
         });
+      }
     }
+  }
 
-    componentDidMount() {
-        if (this.props.src != null) {
-            this.fetchImgByUrl(this.props.src);
-            this.setState({
-                url: null
-            });
-        }
-
-    }
-
-    componentDidUpdate(prevProps: {src: string}) {
-        if (this.props.src !== prevProps.src) {
-            if(this.props.src != null) {
-                this.fetchImgByUrl(this.props.src);
-                this.setState({
-                    url: null
-                });
-            }
-        }
-    }
-
-    render() {
-        return <img alt=""
-            src={this.state.url==null?"":this.state.url} 
-            style={{display:"block", margin:"auto"}}
-            height={`${this.props.height}px`} 
-            width={`${this.props.width}px`}
-        />
-    }
+  render() {
+    return <img alt=""
+      src={this.state.url == null ? "" : this.state.url}
+      style={{ display: "block", margin: "auto" }}
+      height={`${this.props.height}px`}
+      width={`${this.props.width}px`}
+    />
+  }
 }
